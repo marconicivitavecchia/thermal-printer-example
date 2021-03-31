ed83385368824f9a55b60f51a4a5ceca98c7c04e
Claudio <claudio.capobianco@marconicloud.it>
2021-03-31 22:34:49 +0200

    Format product list

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 let app = {
+    productList: [
+        {
+            name: "CAFFEEEEEEEEEEEEEEEEEEEEEEE",
+            quantity: 6,
+            priceUnit: 0.6
+        },
+        {
+            name: "CAPPUCCINO",
+            quantity: 3,
+            priceUnit: 1
+        },
+    ],
+    populateHtml: function () {
+        for (p of app.productList) {
+            const nameText = p.name.padEnd(20, " ").substring(0, 20);
+            const quantityText = p.quantity.toString().padEnd(8, " ").substring(0, 8);
+            const priceTotText = (p.priceUnit * p.quantity).toFixed(2).toString().concat("€").padEnd(8, " ").substring(0, 8)
+            const name = $("<div>").addClass("productCell").text(nameText);
+            const quantity = $("<div>").addClass("productCell").text(quantityText);
+            const priceTot = $("<div>").addClass("productCell").text(priceTotText);
+            const row = $("<div>").addClass("productRow")
+                .append(name)
+                .append(quantity)
+                .append(priceTot);
+            console.log(row);
+            $("#productTable").append(row);
+        }
+    },
     init: function () {
-        // $("#connectButton").click(_ => {});
+
+        app.populateHtml();
+
         var img = new Image();   // Create new img element
         img.addEventListener('load', function () {
             let canvas = document.getElementById('canvas');
             canvas.getContext('2d').drawImage(img, 0, 0);
         }, false);
-        // img.src = 'logo1.bmp'; // from example
-
         img.src = 'small-logo.jpeg';
-        // img.src = 'big-logo.png';
-
 
         $("#printButton").click(__ => {
             var canvas = document.getElementById('canvas');
@@ -34,17 +60,46 @@ let app = {
             }
             function executeAddedCode() {
 
-                // Add simple text
-                printer.addTextAlign(printer.ALIGN_CENTER);
-                printer.addText('IIS Marconi Bar\n');
-
                 // Add image
+                printer.addTextAlign(printer.ALIGN_CENTER);
                 printer.brightness = 1.0;
                 printer.halftone = printer.HALFTONE_DITHER;
                 printer.addImage(canvas.getContext('2d'), 0, 0, canvas.width, canvas.height, printer.COLOR_1, printer.MODE_MONO);
+                printer.addFeedUnit(30);
+
+                // Add title
+                printer.addTextAlign(printer.ALIGN_CENTER);
+                printer.addText('IIS Marconi Bar\n');
+                printer.addFeedUnit(30);
+
+                // Add tab header
+
+                const nameLength = 30;
+                const quantityLength = 6;
+                const pricetTotLength = 10;
+                const nameText = "Prodotto".padEnd(nameLength, " ").substring(0, nameLength);
+                const quantityText = "Quant.".padStart(quantityLength, " ").substring(0, quantityLength);
+                const priceTotText = "Totale".padStart(pricetTotLength, " ").substring(0, pricetTotLength);
+                printer.addTextAlign(printer.ALIGN_LEFT);
+                printer.addText(`${nameText}`);
+                printer.addTextAlign(printer.ALIGN_RIGHT);
+                printer.addText(`${quantityText}`);
+                printer.addText(`${priceTotText}\n`);
+
+                for (p of app.productList) {
+                    const nameText = p.name.padEnd(nameLength, " ").substring(0, nameLength);
+                    const quantityText = p.quantity.toString().padStart(quantityLength, " ").substring(0, quantityLength);
+                    const priceTotText = (p.priceUnit * p.quantity).toFixed(2).toString().concat("€").padStart(pricetTotLength, " ").substring(0, pricetTotLength);
+
+                    printer.addTextAlign(printer.ALIGN_LEFT);
+                    printer.addText(`${nameText}`);
+                    printer.addTextAlign(printer.ALIGN_RIGHT);
+                    printer.addText(`${quantityText}`);
+                    printer.addText(`${priceTotText}\n`);
+                }
 
                 // Feed and cut
-                printer.addFeedUnit(50);
+                printer.addFeedUnit(30);
                 printer.addCut(printer.CUT_FEED);
 
 
